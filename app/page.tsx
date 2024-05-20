4064452608263cb852cf57d83115d2f99538b600
KobeNorcross <kobenorcross@gmail.com>
2024-05-20 16:02:40 -0700

    Starting to style landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,22 +5,23 @@ export default function Home() {
   return (
     <main className="h-full flex gap-4 flex-col items-center align-items-center p-24">
       <h1>This is the landing page</h1>
-      <div className="border border-white rounded-xl">
-        <SignInButton
-          fallbackRedirectUrl="/dashboard"
-          signUpFallbackRedirectUrl="/onboarding"
-        >
-          Sign in
-        </SignInButton>
-      </div>
-
-      <div className="border border-white rounded-xl">
-        <SignUpButton
-          signInFallbackRedirectUrl="/dashboard"
-          fallbackRedirectUrl="/onboarding"
-        >
-          Sign up
-        </SignUpButton>
+      <div className="flex p-5">
+        <div className="border border-pink-500 bg-cyan-700 text-cyan-100 rounded-xl m-5 p-2">
+          <SignInButton
+            fallbackRedirectUrl="/dashboard"
+            signUpFallbackRedirectUrl="/onboarding"
+          >
+            Sign in
+          </SignInButton>
+        </div>
+        <div className="border border-pink-500 bg-cyan-700 text-cyan-100 rounded-xl m-5 p-2">
+          <SignUpButton
+            signInFallbackRedirectUrl="/dashboard"
+            fallbackRedirectUrl="/onboarding"
+          >
+            Sign up
+          </SignUpButton>
+        </div>
       </div>
     </main>
   );
