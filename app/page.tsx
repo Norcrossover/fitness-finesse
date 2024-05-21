3c677de58af6555be70f36482a01e884a8b868e1
KobeNorcross <kobenorcross@gmail.com>
2024-05-20 18:14:50 -0700

    Last update to content

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,15 @@ export default function Home() {
         <h1 className="text-2xl text-center">Welcome to Fitness Finesse</h1>
       </div>
       <div className="row-span-8 grid grid-rows-3 gap-4 size-full">
-        <div className="row-span-1 bg-blue-200 p-5">Content 1</div>
-        <div className="row-span-1 bg-blue-300 p-5">Content 2</div>
-        <div className="row-span-1 bg-blue-400 p-5">Content 3</div>
+        <div className="row-span-1 bg-blue-200 p-5 shadow-lg rounded-lg">
+          Content 1
+        </div>
+        <div className="row-span-1 bg-blue-300 p-5 shadow-lg rounded-lg">
+          Content 2
+        </div>
+        <div className="row-span-1 bg-blue-400 p-5 shadow-lg rounded-lg">
+          Content 3
+        </div>
       </div>
       <div className="row-span-2 flex justify-center p-5 w-full">
         <AuthenticationButtonStyling>
