08ea5c9f3adae8469c26f22a9a4aa2fcf9ef7413
Victor Castro <vicastro@pdx.edu>
2024-05-24 11:32:44 -0700

    Added motion blur to header component

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from "./ThemeToggle";
 
 const Header = () => {
   return (
-    <header className="dark:bg-slate-800 bg-slate-400 sticky top-0 z-20 mx-auto flex flex-wrap w-full items-center justify-between p-4">
+    <header className="bg-opacity-80 backdrop-blur-lg backdrop-brightness-100 dark:bg-slate-800 dark:bg-opacity-80 bg-slate-400 sticky top-0 z-20 mx-auto flex flex-wrap w-full items-center justify-between p-4">
       <Logo />
       <ThemeToggle />
       <NavBar />
