f057fb3931ae3345c2a55e9b320de859b26c13c5
KobeNorcross <kobenorcross@gmail.com>
2024-05-28 11:39:42 -0700

    Added some placeholder content for the dashboard and updated sign in verifcation to be nextjs

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,43 +1,36 @@
 "use client";
 import React from "react";
-import {
-  RedirectToSignIn,
-  SignedIn,
-  SignedOut,
-  UserButton,
-  useUser,
-} from "@clerk/clerk-react";
-import { useRouter } from "next/navigation";
+import { RedirectToSignIn, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 
 const Dashboard = () => {
-  const { isSignedIn, isLoaded } = useUser();
-  const router = useRouter();
-
-  React.useEffect(() => {
-    if (!isLoaded) return; // Wait until user loading state is determined
-    if (!isSignedIn) {
-      router.push("/sign-in"); // Redirect if not signed in
-    }
-  }, [isSignedIn, isLoaded, router]);
+  const { user } = useUser();
 
   return (
     <div>
-      {/* Dashboard content */}
-      {/* {isSignedIn && <p>Welcome to your Dashboard!</p>} */}
       <SignedOut>
-        <RedirectToSignIn></RedirectToSignIn>
+        <RedirectToSignIn />
       </SignedOut>
       <SignedIn>
-        <p>Welcome to your Dashboard!</p>
-        <UserButton
-          showName={true}
-          appearance={{
-            elements: {
-              formButtonPrimary:
-                "bg-slate-500 hover:bg-slate-400 text-sm normal-case",
-            },
-          }}
-        />
+        <main className="h-full w-full grid grid-rows-12 gap-4 items-center justify-items-center p-4 m-4">
+          <div className="row-span-2 flex-col items-center w-full font-semibold">
+            <h1 className="text-2xl">Good</h1>
+            <h1 className="text-2xl">afternoon,</h1>
+            <h1 className="text-2xl">
+              {user?.fullName ? user?.firstName : user?.username}.
+            </h1>
+          </div>
+          <div className="row-span-8 grid grid-rows-3 gap-4 size-full">
+            <div className="row-span-1 bg-cyan-200 p-5 shadow-lg rounded-lg h-[250px]">
+              These will
+            </div>
+            <div className="row-span-1 bg-cyan-300 p-5 shadow-lg rounded-lg h-[250px]">
+              Be replaced by
+            </div>
+            <div className="row-span-1 bg-cyan-400 p-5 shadow-lg rounded-lg h-[250px]">
+              Chart Content that shows the Workout data for the current user
+            </div>
+          </div>
+        </main>
       </SignedIn>
     </div>
   );
