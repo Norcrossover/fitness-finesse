39082da0c3888be59938dfbbb2be797a71f7ab48
KobeNorcross <kobenorcross@gmail.com>
2024-05-28 12:33:51 -0700

    Added motivational message to user on sign up

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,10 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center align-center p-10 m-10">
+    <div className="flex flex-col items-center justify-center p-10 m-10 min-h-1/2">
+      <h1 className="text-2xl font-semibold text-center m-4 p-4">
+        We{"'"}re glad to see you join us in this journey
+      </h1>
       <SignUp path="/sign-up" />
     </div>
   );
