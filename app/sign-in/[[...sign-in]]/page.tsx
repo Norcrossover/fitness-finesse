fb2be9d1982d04852261f83f9e8ea02f45fa28e9
KobeNorcross <kobenorcross@gmail.com>
2024-05-28 11:40:06 -0700

    Stylized the signin/up pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn path="/sign-in" />;
+  return (
+    <div className="flex justify-center align-center p-10 m-10">
+      <SignIn path="/sign-in" />
+    </div>
+  );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp path="/sign-up" />;
+  return (
+    <div className="flex justify-center align-center p-10 m-10">
+      <SignUp path="/sign-up" />
+    </div>
+  );
 }
