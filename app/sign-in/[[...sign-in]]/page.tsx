ccf45023aea151903c26730f46e5df30bed197a6
KobeNorcross <kobenorcross@gmail.com>
2024-05-29 16:46:10 -0700

    Updated Sign-in page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,10 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center align-center p-10 m-10">
+    <div className="flex flex-col items-center justify-center p-5 m-5 md:h-[1000px] min-h-3/4">
+      <h1 className="text-2xl font-semibold text-center m-4 p-4">
+        Welcome back!
+      </h1>
       <SignIn path="/sign-in" />
     </div>
   );
