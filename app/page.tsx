964cdb0523b9a0d7011466f8ce8cb7cea7b7e593
KobeNorcross <kobenorcross@gmail.com>
2024-05-29 18:26:12 -0700

    Update h1 of Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <main className="h-full w-full grid grid-rows-12 gap-4 items-center justify-items-center p-4">
       <div className="row-span-1 flex items-center justify-center w-full">
-        <h1 className="text-2xl text-center">Welcome to Fitness Finesse</h1>
+        <h1 className="text-3xl font-bold text-center">
+          Welcome to Fitness Finesse
+        </h1>
       </div>
       <div className="row-span-10 grid grid-rows-3 gap-4 size-full">
         <div className="row-span-1 bg-cyan-200 p-5 shadow-lg rounded-lg">
