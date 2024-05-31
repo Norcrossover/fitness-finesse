f81c22566d40d5da9a7054509f4112a24c4edeee
KobeNorcross <kobenorcross@gmail.com>
2024-05-30 18:00:01 -0700

    Created models for WorkoutLog and function to make timestamp readable

--- models/workout.ts
+++ models/workout.ts
@@ -1,3 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-interface IWorkout {}
+export type ExerciseLog = {
+  name: string;
+  reps: number;
+  sets: number;
+};
+
+export interface IWorkoutLog extends Document {
+  id: number;
+  exerciseList: ExerciseLog[];
+}
+
+const exerciseLogSchema = new Schema<ExerciseLog>({
+  name: { type: String, required: true },
+  reps: { type: Number, required: true },
+  sets: { type: Number, required: true },
+});
+
+const workoutLogSchema = new Schema<IWorkoutLog>(
+  {
+    id: { type: Number, required: true },
+    exerciseList: [exerciseLogSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const WorkoutLog =
+  mongoose.models.WorkoutLog ||
+  mongoose.model<IWorkoutLog>("WorkoutLog", workoutLogSchema);
+
+export default WorkoutLog;
