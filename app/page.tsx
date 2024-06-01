dc9652280006d37639f8c39398efef8a237852e4
KobeNorcross <kobenorcross@gmail.com>
2024-06-01 13:09:23 -0700

    Updated landing page to redirect users to the dashboard if they are signed in

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,66 @@
-import { SignInButton, SignUpButton } from "@clerk/nextjs";
+"use client";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  useAuth,
+} from "@clerk/nextjs";
 import AuthenticationButtonStyling from "@/components/common/authenticationButtonStyling";
 import ExercisePageSnippet from "@/components/features/landingPage/ExercisesPageSnippet";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const { isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isSignedIn, router]);
+
   return (
-    <main className="h-full w-full grid grid-rows-12 gap-4 items-center justify-items-center p-4">
-      <div className="row-span-1 flex items-center justify-center w-full">
-        <h1 className="text-3xl font-bold text-center">
-          Welcome to Fitness Finesse
-        </h1>
-      </div>
-      <div className="row-span-10 grid grid-rows-3 gap-4 size-full">
-        <div className="row-span-1 bg-cyan-200 p-5 shadow-lg rounded-lg">
-          <ExercisePageSnippet />
-        </div>
-        <div className="row-span-1 bg-cyan-300 p-5 shadow-lg rounded-lg">
-          Content 2
-        </div>
-        <div className="row-span-1 bg-cyan-400 p-5 shadow-lg rounded-lg">
-          Content 3
-        </div>
-      </div>
-      <div className="row-span-1 flex justify-center p-5 w-full">
-        <AuthenticationButtonStyling>
-          <SignInButton
-            fallbackRedirectUrl="/dashboard"
-            signUpFallbackRedirectUrl="/onboarding"
-          >
-            Log in
-          </SignInButton>
-        </AuthenticationButtonStyling>
-        <AuthenticationButtonStyling>
-          <SignUpButton
-            signInFallbackRedirectUrl="/dashboard"
-            fallbackRedirectUrl="/onboarding"
-          >
-            Sign up
-          </SignUpButton>
-        </AuthenticationButtonStyling>
-      </div>
-    </main>
+    <>
+      <SignedOut>
+        <main className="h-full w-full grid grid-rows-12 gap-4 items-center justify-items-center p-4">
+          <div className="row-span-1 flex items-center justify-center w-full">
+            <h1 className="text-3xl font-bold text-center">
+              Welcome to Fitness Finesse
+            </h1>
+          </div>
+          <div className="row-span-10 grid grid-rows-3 gap-4 size-full">
+            <div className="row-span-1 bg-cyan-200 p-5 shadow-lg rounded-lg">
+              <ExercisePageSnippet />
+            </div>
+            <div className="row-span-1 bg-cyan-300 p-5 shadow-lg rounded-lg">
+              Content 2
+            </div>
+            <div className="row-span-1 bg-cyan-400 p-5 shadow-lg rounded-lg">
+              Content 3
+            </div>
+          </div>
+          <div className="row-span-1 flex justify-center p-5 w-full">
+            <AuthenticationButtonStyling>
+              <SignInButton
+                fallbackRedirectUrl="/dashboard"
+                signUpFallbackRedirectUrl="/onboarding"
+              >
+                Log in
+              </SignInButton>
+            </AuthenticationButtonStyling>
+            <AuthenticationButtonStyling>
+              <SignUpButton
+                signInFallbackRedirectUrl="/dashboard"
+                fallbackRedirectUrl="/onboarding"
+              >
+                Sign up
+              </SignUpButton>
+            </AuthenticationButtonStyling>
+          </div>
+        </main>
+      </SignedOut>
+    </>
   );
 }
