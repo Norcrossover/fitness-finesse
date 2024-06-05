4727a8c56011cd1ffb0d89bafe14a7a9d94241cd
Victor Castro <vicastro@pdx.edu>
2024-06-04 21:59:38 -0700

    Integrated the loading component to charts

--- components/features/dashboard/Loading.tsx
+++ components/features/dashboard/Loading.tsx
@@ -5,8 +5,8 @@ type LoadingProps = {
 const Loading = ({ text }: LoadingProps) => {
   return (
     <div>
-      <p className="text-xl  p-4 sm:text-2xl sm:font-bold text-center">
-        {text} <span className="text-cyan animate-pulse">exercise</span> page!{" "}
+      <p className="text-xl p-4 sm:text-2xl font-bold text-center">
+        {text} <span className="text-cyan animate-pulse">exercise</span> page!
       </p>
     </div>
   );
